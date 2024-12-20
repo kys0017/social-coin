96b6b91c0250d0bc7c16e94b04e1c805ef92ea12
kys0017 <kys0017@gmail.com>
2024-12-20 20:56:51 +0900

    ✨ 6.1 Android Setup - 6.2 iOS Setup

--- app/index.tsx
+++ app/index.tsx
@@ -2,8 +2,8 @@ import { Text, View } from 'react-native';
 
 export default function HomeScreen() {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ color: 'red', fontSize: 30 }}>Home!!!plz</Text>
     </View>
   );
 }
